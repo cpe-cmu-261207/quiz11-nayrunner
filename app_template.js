9259fcf4a4699fd83f9cdcabcb8e2e2f38814aa1
nayrunner <Nutchanon_thonganun@cmu.co.th>
2021-01-12 13:49:37 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,18 @@
 //your code here!
+function MinusId(){
+    document.addEventListener('DOMContentLoaded',function(){
+        document.querySelector('#button-compute').onsubmit = function(){
+            const space = document.createElement('tr')
+            const sid = document.querySelector('#input-number').value
+            const doIt = document.querySelector('#button-compute')
+
+            let calculate = 620610785 - sid
+
+            doIt.append(space)
+            doIt.append(calculate)
+            doIt.append(space)
+            
+
+        }
+    })
+}
